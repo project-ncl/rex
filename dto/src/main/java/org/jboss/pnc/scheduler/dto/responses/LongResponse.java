f0dd222381675e1b2e815641d1e7df2a34350d07
Jan Michalov <jmichalo@redhat.com>
2021-04-21 19:11:34 +0200

    dto cleanup

--- dto/src/main/java/org/jboss/pnc/scheduler/dto/responses/LongResponse.java
+++ dto/src/main/java/org/jboss/pnc/scheduler/dto/responses/LongResponse.java
@@ -4,15 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.jboss.pnc.scheduler.dto.TaskDTO;
-
-import java.util.List;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class TaskListResponse {
-
-    public List<TaskDTO> taskDTOS;
+public class LongResponse {
+    public Long number;
 }
