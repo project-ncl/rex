005c1ad58231bc8122c757dffd39ce174de84c4b
Jan Michalov <jmichalo@redhat.com>
2021-07-20 18:12:26 +0200

    update to quarkus 2.0.2.Final and improve maven process

--- core/src/main/java/org/jboss/pnc/scheduler/core/GenericVertxHttpClient.java
+++ core/src/main/java/org/jboss/pnc/scheduler/core/GenericVertxHttpClient.java
@@ -24,24 +24,57 @@ public class GenericVertxHttpClient {
         this.client = WebClient.create(vertx);
     }
 
+    /**
+     * Must be ran on a thread that can block
+     * @param remoteEndpoint
+     * @param method
+     * @param headers
+     * @param requestBody
+     * @param onResponse
+     */
     public void makeRequest(URI remoteEndpoint,
                              Method method,
                              List<Header> headers,
                              Object requestBody,
                              Consumer<HttpResponse<Buffer>> onResponse) {
-        HttpRequest<Buffer> request = client.raw(method.toString(),
+        HttpRequest<Buffer> request = client.request(toVertxMethod(method),
                 remoteEndpoint.getPort() == -1 ? 80 : remoteEndpoint.getPort(),
                 remoteEndpoint.getHost(),
                 remoteEndpoint.getPath());
         addHeaders(request, headers);
 
-        request.sendJson(requestBody)
-                .onItem().transformToUni(i ->
-                    Uni.createFrom()
-                        .item(i).onItem().invoke(onResponse)
-                        .onFailure().retry().atMost(5))
-                .onFailure().retry().atMost(5)
-                .subscribe().with(resp -> {});
+        log.trace("HTTP-CLIENT : Making request \n URL: {}\n METHOD: {}\n HEADERS: {}\n BODY: {}",
+                remoteEndpoint,
+                method,
+                headers.toString(),
+                requestBody.toString());
+
+        Uni.createFrom().item(request.sendJsonAndAwait(requestBody))
+            .onItem().transformToUni(i -> Uni.createFrom()
+                .item(i).onItem().invoke(onResponse)
+                .onFailure().retry().atMost(5))
+            .onFailure().retry().atMost(5)
+            .await().indefinitely();
+    }
+
+    private HttpMethod toVertxMethod(Method method) {
+        switch (method) {
+            case GET:
+                return HttpMethod.GET;
+            case POST:
+                return HttpMethod.POST;
+            case PUT:
+                return HttpMethod.PUT;
+            case PATCH:
+                return HttpMethod.PATCH;
+            case DELETE:
+                return HttpMethod.DELETE;
+            case HEAD:
+                return HttpMethod.HEAD;
+            case OPTIONS:
+                return HttpMethod.OPTIONS;
+        }
+        return null;
     }
 
     private void addHeaders(HttpRequest<Buffer> request, List<Header> headers) {
