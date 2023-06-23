1e489113e9f7d9ffd010c1e142090320cc26ffd6
jmichalo <jmichalo@redhat.com>
2023-06-23 11:41:00 +0200

    create a seperate VersionEndpoint API and move implementation to rest

--- core/src/main/java/org/jboss/pnc/rex/rest/VersionEndpointImpl.java
+++ core/src/main/java/org/jboss/pnc/rex/rest/VersionEndpointImpl.java
@@ -15,24 +15,37 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.pnc.rex.api;
+package org.jboss.pnc.rex.rest;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.jboss.pnc.api.dto.ComponentVersion;
+import org.jboss.pnc.rex.api.VersionEndpoint;
+import org.jboss.pnc.rex.common.Constants;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.time.ZonedDateTime;
 
 @Tag(name = "Version Endpoint")
 @Path("/rest/version")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-public interface VersionEndpoint {
+public class VersionEndpointImpl implements VersionEndpoint {
 
-    @GET
-    ComponentVersion getVersion();
+    @ConfigProperty(name = "quarkus.application.name")
+    String name;
 
+    @Override
+    public ComponentVersion getVersion() {
+        return ComponentVersion.builder()
+                .name(name)
+                .version(Constants.REX_VERSION)
+                .commit(Constants.COMMIT_HASH)
+                .builtOn(ZonedDateTime.parse(Constants.BUILD_TIME))
+                .build();
+    }
 }
