7d17554ef5d8962d72149e9b957ff15955089669
jmichalo <jmichalo@redhat.com>
2023-06-26 18:16:07 +0200

    fix tests stability

--- core/src/main/java/org/jboss/pnc/rex/core/TaskListener.java
+++ core/src/main/java/org/jboss/pnc/rex/core/TaskListener.java
@@ -91,4 +91,7 @@ public class TaskListener {
     void failureListener(@Observes(during = TransactionPhase.AFTER_FAILURE) @BeforeDestroyed(TransactionScoped.class) @Priority(APPLICATION + 499) Object ignore) throws SystemException {
         log.error("AFTER FAILURE: Transaction failed " + tm.getTransaction().toString());
     }
+    void successListener(@Observes(during = TransactionPhase.AFTER_SUCCESS) @BeforeDestroyed(TransactionScoped.class) @Priority(APPLICATION + 499) Object ignore) throws SystemException {
+        log.trace("AFTER SUCCESS: Transaction successful " + tm.getTransaction().toString());
+    }
 }
