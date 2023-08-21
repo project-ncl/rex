65843a6edd41f09d224f2d93b649d1ff6933e9a7
jmichalo <jmichalo@redhat.com>
2023-08-21 19:56:51 +0200

    add licenses + fix test

--- core/src/test/java/org/jboss/pnc/rex/core/GenericHttpClientTest.java
+++ core/src/test/java/org/jboss/pnc/rex/core/GenericHttpClientTest.java
@@ -1,3 +1,20 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2021-2021 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.pnc.rex.core;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -7,9 +24,13 @@ import org.jboss.pnc.rex.common.enums.Mode;
 import org.jboss.pnc.rex.common.enums.State;
 import org.jboss.pnc.rex.core.common.TestData;
 import org.jboss.pnc.rex.core.common.TransitionRecorder;
+import org.jboss.pnc.rex.core.counter.Counter;
+import org.jboss.pnc.rex.core.counter.MaxConcurrent;
+import org.jboss.pnc.rex.core.counter.Running;
 import org.jboss.pnc.rex.core.endpoints.HttpEndpoint;
 import org.jboss.pnc.rex.dto.requests.CreateGraphRequest;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
@@ -34,6 +55,21 @@ public class GenericHttpClientTest {
 
     @Inject
     TransitionRecorder recorder;
+
+
+    @Inject
+    @Running
+    Counter running;
+
+    @Inject
+    @MaxConcurrent
+    Counter max;
+
+    @BeforeEach
+    public void before() throws Exception {
+        max.initialize(1000L);
+        running.initialize(0L);
+    }
 
     @AfterEach
     public void after() throws InterruptedException {
