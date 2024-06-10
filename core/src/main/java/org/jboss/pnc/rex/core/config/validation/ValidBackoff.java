49586bce2bb4d6b5957d96ae9d3cfb7e1ca4b3f5
jmichalo <jmichalo@redhat.com>
2024-06-10 15:16:09 +0200

    add constraint validator for backoff max/min value

---
+++ core/src/main/java/org/jboss/pnc/rex/core/config/validation/ValidBackoff.java
@@ -0,0 +1,41 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2021-2021 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.rex.core.config.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({ TYPE, ANNOTATION_TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = ValidBackoffValidator.class)
+@Documented
+public @interface ValidBackoff {
+    String message() default "min-delay cannot be 0 if max-delay is non-zero.";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default {};
+}
